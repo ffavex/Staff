15688effb65755eb30cf547d1540017e71681355
Efial <84471368+Efial@users.noreply.github.com>
2024-03-13 01:13:19 -0400

    Update FallenPub.lua

--- Scripts/FallenPub.lua
+++ Scripts/FallenPub.lua
@@ -1,7 +1,3 @@
 --** Eclipse | Fallen Public **--
 
-if game.PlaceId == 10228136016 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua"))()
-else
-    game.Players.LocalPlayer:Kick("Please make sure you inject into lobby to ensure you do NOT get banned!!!")
-end
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua"))()
