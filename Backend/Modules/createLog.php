9b74a906f9c8c932a6dfd7fc6a2d1758eeb77606
Efial <84471368+Efial@users.noreply.github.com>
2024-04-05 07:37:25 -0400

    Delete Backend directory

--- Backend/Modules/createLog.php
+++
@@ -1,5 +0,0 @@
-<?php
-function createLog($str){
-      file_put_contents("./logs/secure-log", "\n".date('h:i:s A, m/d/Y')."|".$str, FILE_APPEND | LOCK_EX);
-};
-?>
--- Backend/Modules/isWithin.php
+++
@@ -1,5 +0,0 @@
-<?php
-function isWithin($number){
-  if ($number == 1 or $number == 0){return true;}else{return false;};
-};
-?>
