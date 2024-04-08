c51d474ac5610705fdadada7af09faab546fc7c2
Efial <84471368+Efial@users.noreply.github.com>
2024-04-08 14:23:43 -0400

    Update FallenPub.lua

--- Scripts/FallenPub.lua
+++ Scripts/FallenPub.lua
@@ -1,3 +1,8 @@
---** Eclipse | Fallen Public **--
+repeat wait() until game:IsLoaded()
+local place_id = game.PlaceId;
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua"))()
+if (place_id == 10179538382) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d2dfa4cebd0410e773e815204a8edbe4.lua", true))()
+elseif (place_id == 10228136016) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua", true))()
+end
