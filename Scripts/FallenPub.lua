a6518142d49f959bcfac524928ac902a3b16211c
Efial <84471368+Efial@users.noreply.github.com>
2024-04-10 07:04:05 -0400

    Update FallenPub.lua

--- Scripts/FallenPub.lua
+++ Scripts/FallenPub.lua
@@ -3,6 +3,6 @@ local creator_id = game.CreatorId;
 
 if (creator_id == 14032806) then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d2dfa4cebd0410e773e815204a8edbe4.lua", true))()
-elseif (creator_id == 10228136016) then
+elseif (creator_id == 1154360 and game.PlaceId ~= 10228136016) then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua", true))()
 end
