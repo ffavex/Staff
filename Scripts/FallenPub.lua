bef67b5c15505afea4f823af5797e9939a02b581
Efial <84471368+Efial@users.noreply.github.com>
2024-04-12 15:28:10 -0400

    Update FallenPub.lua

--- Scripts/FallenPub.lua
+++ Scripts/FallenPub.lua
@@ -1,11 +1,11 @@
 repeat wait() until game:IsLoaded()
 local place_id = game.PlaceId;
 
-if (place_id == 10179538382) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d2dfa4cebd0410e773e815204a8edbe4.lua", true))()
-elseif (place_id == 10228136016) then
-    queue_on_teleport([[
+if (place_id == 10228136016) then
+    pcall(function()
         repeat wait() until game:GetService("Players").LocalPlayer.Character
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua", true))()
-    ]])
+    end);
+elseif (place_id == 10179538382) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d2dfa4cebd0410e773e815204a8edbe4.lua", true))()
 end
