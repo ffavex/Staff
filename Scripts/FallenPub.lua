c08acac18147b0677e20f6d6590dff7cbd1cd68d
Efial <84471368+Efial@users.noreply.github.com>
2024-04-12 16:18:01 -0400

    Update FallenPub.lua

--- Scripts/FallenPub.lua
+++ Scripts/FallenPub.lua
@@ -1,8 +1,16 @@
-repeat wait() until game:IsLoaded()
-local place_id = game.PlaceId;
+local FallenId = 10228136016
+writefile("ECLIPSE_KEY.txt", script_key)
 
-if (place_id == 10228136016) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfee51375d5fb9484aff5f92dac805a5.lua", true))()
-elseif (place_id == 10179538382) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d2dfa4cebd0410e773e815204a8edbe4.lua", true))()
-end
+if game.PlaceId == FallenId then
+    queue_on_teleport([[
+        pcall(function()
+            script_key = readfile("ECLIPSE_KEY.txt")
+            repeat task.wait() until getactors()[1] and getactors()[2] 
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/BurnHubz/eclipse/main/checkcaller.lua"))();
+            task.wait(1);
+            memorystats.cache("Gui")
+            task.wait();
+            loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2e293addcf9a470164fe95eff5e92fc.lua"))()
+        end)
+    ]])
+end;
